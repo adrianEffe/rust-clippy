98b82cf2ca1efd987cf174fd2d389d8d59d3f0b6
adrianEffe <adrian.iosdev@gmail.com>
2023-05-21 17:20:52 +0300

    Add span lint and help

--- clippy_lints/src/foo_functions.rs
+++ clippy_lints/src/foo_functions.rs
@@ -1,6 +1,9 @@
 use rustc_ast::ast::*;
 use rustc_lint::{EarlyContext, EarlyLintPass};
 use rustc_session::{declare_lint_pass, declare_tool_lint};
+use clippy_utils::diagnostics::span_lint_and_help;
+use rustc_span::Span;
+use rustc_ast::visit::FnKind;
 
 declare_clippy_lint! {
     /// ### What it does
@@ -22,4 +25,15 @@ declare_clippy_lint! {
 }
 declare_lint_pass!(FooFunctions => [FOO_FUNCTIONS]);
 
-impl EarlyLintPass for FooFunctions {}
+impl EarlyLintPass for FooFunctions {
+    fn check_fn(&mut self, cx: &EarlyContext<'_>, fn_kind: FnKind<'_>, span: Span, _: NodeId) {
+        span_lint_and_help(
+            cx,
+            FOO_FUNCTIONS,
+            span,
+            "function named `foo`",
+            None,
+            "consider using a more meaningful name"
+        );
+    }
+}
